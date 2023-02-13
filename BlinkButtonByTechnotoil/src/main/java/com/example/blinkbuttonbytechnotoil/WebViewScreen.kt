115e2c2b49a4943691d75985bce102294dce4cbd
rajvision10 <rajvision10@mail.com>
2023-02-13 14:07:28 +0530

    Improved webview

--- BlinkButtonByTechnotoil/src/main/java/com/example/blinkbuttonbytechnotoil/WebViewScreen.kt
+++ BlinkButtonByTechnotoil/src/main/java/com/example/blinkbuttonbytechnotoil/WebViewScreen.kt
@@ -5,12 +5,18 @@ import android.webkit.WebView
 import androidx.appcompat.app.AppCompatActivity
 import android.webkit.WebViewClient
 
-class WebViewScreen : AppCompatActivity() {
+class WebViewScreen(url: String) : AppCompatActivity() {
+    val _url: String
+
+    init {
+        _url = url
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.webiew_layout)
 
-
+        showwebview(_url);
     }
 
     fun showwebview(url: String) {
