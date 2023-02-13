071ba5897620234aea9c2a3f643fa0f8f7f23d54
rajvision10 <rajvision10@mail.com>
2023-02-13 17:39:51 +0530

    Improved webviewss

--- BlinkButtonByTechnotoil/src/main/java/com/example/blinkbuttonbytechnotoil/Webviewscreen.kt
+++ BlinkButtonByTechnotoil/src/main/java/com/example/blinkbuttonbytechnotoil/Webviewscreen.kt
@@ -6,13 +6,14 @@ import android.webkit.WebSettings
 import android.webkit.WebView
 import androidx.appcompat.app.AppCompatActivity
 
-class Webviewscreen: Activity() {
+class Webviewscreen(url: String) : Activity() {
+    var _url = url
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val theWebPage = WebView(this)
         theWebPage.settings.javaScriptEnabled = true
         theWebPage.settings.pluginState = WebSettings.PluginState.ON
         setContentView(theWebPage)
-        theWebPage.loadUrl("https://www.geeksforgeeks.org")
+        theWebPage.loadUrl(_url)
     }
 }
