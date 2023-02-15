d8027c6731a467cabc837f840f31f9d87f5d4fd1
rajvision10 <rajvision10@mail.com>
2023-02-15 19:53:53 +0530

    permission issue resolved

--- BlinkButtonByTechnotoil/src/main/java/com/example/blinkbuttonbytechnotoil/Webviewscreen.kt
+++ BlinkButtonByTechnotoil/src/main/java/com/example/blinkbuttonbytechnotoil/Webviewscreen.kt
@@ -1,7 +1,11 @@
 package com.example.blinkbuttonbytechnotoil
 
+import android.annotation.TargetApi
 import android.app.Activity
+import android.os.Build
 import android.os.Bundle
+import android.webkit.PermissionRequest
+import android.webkit.WebChromeClient
 import android.webkit.WebSettings
 import android.webkit.WebView
 import androidx.appcompat.app.AppCompatActivity
@@ -12,6 +16,12 @@ class Webviewscreen: Activity() {
         val theWebPage = WebView(this)
         theWebPage.settings.javaScriptEnabled = true
         theWebPage.settings.pluginState = WebSettings.PluginState.ON
+        theWebPage.setWebChromeClient(object : WebChromeClient() {
+            @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+            override fun onPermissionRequest(request: PermissionRequest) {
+                request.grant(request.resources)
+            }
+        })
         setContentView(theWebPage)
         theWebPage.loadUrl("https://cdn.camweara.com/camweara_jewelry_client/index.php?skus=earring6&company_name=Teststore")
     }
