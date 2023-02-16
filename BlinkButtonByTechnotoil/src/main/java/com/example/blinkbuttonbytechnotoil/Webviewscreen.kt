3744f133e6f3bbdbca5ee66126c4740ca1a9cef2
rajvision10 <rajvision10@mail.com>
2023-02-16 14:12:37 +0530

    add text field

--- BlinkButtonByTechnotoil/src/main/java/com/example/blinkbuttonbytechnotoil/Webviewscreen.kt
+++ BlinkButtonByTechnotoil/src/main/java/com/example/blinkbuttonbytechnotoil/Webviewscreen.kt
@@ -9,16 +9,17 @@ import android.webkit.WebChromeClient
 import android.webkit.WebSettings
 import android.webkit.WebView
 
-class Webviewscreen: Activity() {
-    var data = "FirstActivity"
-    var INSTANCE: Webviewscreen? = null
+class Webviewscreen : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        INSTANCE=this;
         super.onCreate(savedInstanceState)
         val theWebPage = WebView(this)
+
+        val url: String? = intent.getStringExtra("url")
+
         theWebPage.settings.javaScriptEnabled = true
         theWebPage.settings.pluginState = WebSettings.PluginState.ON
+
         theWebPage.setWebChromeClient(object : WebChromeClient() {
             @TargetApi(Build.VERSION_CODES.LOLLIPOP)
             override fun onPermissionRequest(request: PermissionRequest) {
@@ -26,14 +27,8 @@ class Webviewscreen: Activity() {
             }
         })
         setContentView(theWebPage)
-        theWebPage.loadUrl("https://cdn.camweara.com/camweara_jewelry_client/index.php?skus=demo_1020054,demo_panerai1&company_name=Teststore&1676527013359")
-    }
-    fun getActivityInstance(): Webviewscreen? {
-        return INSTANCE
-    }
-
-    @JvmName("getData1")
-    fun getData(): String? {
-        return data
+        if (url != null) {
+            theWebPage.loadUrl(url)
+        }
     }
 }
